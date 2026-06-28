138ddb6357f910f3d08be79f4453739bd54819c0
gabriel03013 <gm5770283@gmail.com>
2026-06-28 15:31:38 -0300

    refactor: update GenericExceptionResponse with ErrorStatusCode and optional timestamp

--- src/exceptions/exception-response.dto.ts
+++ src/exceptions/exception-response.dto.ts
@@ -1,11 +1,11 @@
-import type { ErrorStatus } from "../types/error-status.type.js";
+import type { ErrorStatusCode } from "../types/error-status.type.js";
 import type { FieldError } from "../types/fields-error.type.js";
 
 export interface GenericExceptionResponse {
-  status: ErrorStatus;
+  status: ErrorStatusCode;
   code: string;
   message: string;
   displayMessage?: string;
   fields?: FieldError[];
-  timestamp: string;
+  timestamp?: string;
 }
